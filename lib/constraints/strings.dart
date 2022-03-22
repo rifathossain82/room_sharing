93382f4b4e332ebaa4d030d2a8755f927f1931b7
rifathossain82 <officialrifat82@gmail.com>
2022-03-22 08:47:47 +0600

    create list of division and district

--- lib/constraints/strings.dart
+++ lib/constraints/strings.dart
@@ -9,3 +9,135 @@ String linkedin_url='https://www.linkedin.com/in/rifat-hossain-66811b201/';
 String github_url='https://github.com/rifathossain82';
 String fiverr_url='https://www.fiverr.com/rifathossain82?up_rollout=true';
 String youTube_url='https://www.youtube.com/channel/UCHQjehNirRNYmWtYK0rh9jg';
+
+
+List<String> divisionList=[
+  'Dhaka',
+  'Chattogram',
+  'Khulna',
+  'Barishal',
+  'Mymensingh',
+  'Rajshahi',
+  'Rangpur',
+  'Sylhet',
+];
+
+List<String> barishalDistrictList=[
+  'Barguna',
+  'Barishal,',
+  'Bhola',
+  'Jhalokathi',
+  'Patuakhali',
+  'Pirojpur',
+];
+
+List<String> chittagongDistrictList=[
+  'B.baria	',
+  'Bandarban,',
+  'Chandpur',
+  'Chattogram',
+  'Cox\'s bazar',
+  'Cumilla',
+  'Feni',
+  'Khagrachari',
+  'Laxmipur',
+  'Noakhali',
+  'Rangamati',
+];
+
+
+List<String> dhakaDistrictList=[
+  'Dhaka',
+  'Faridpur,',
+  'Gazipur',
+  'Gopalganj',
+  'Kishoreganj',
+  'Madaripur',
+  'Manikganj',
+  'Munshiganj',
+  'Narayanganj',
+  'Narshingdi',
+  'Rajbari',
+  'Shariatpur',
+  'Tangail',
+];
+
+List<String> khulnaDistrictList=[
+  'Bagerhat',
+  'Chuadanga,',
+  'Jashore',
+  'Jhenaidah',
+  'Khulna',
+  'Kushtia',
+  'Magura',
+  'Meherpur',
+  'Narail',
+  'Satkhira',
+];
+
+List<String> mymensinghDistrictList=[
+  'Jamalpur',
+  'Mymensingh,',
+  'Netrokona',
+  'Sherpur',
+];
+
+List<String> rajshahiDistrictList=[
+  'Bogura',
+  'C. nawabganj',
+  'Joypurhat',
+  'Naogaon',
+  'Natore',
+  'Pabna',
+  'Rajshahi',
+  'Sirajganj',
+];
+
+List<String> rangpurDistrictList=[
+  'Dinajpur',
+  'Gaibandha',
+  'Kurigram',
+  'Lalmonirhat',
+  'Nilphamari',
+  'Panchagarh',
+  'Rangpur',
+  'Thakurgaon',
+];
+
+List<String> sylhetDistrictList=[
+  'Habiganj',
+  'Moulvibazar',
+  'Sunamganj',
+];
+
+List<String> bargunaUpazilaList=[
+  'Amtali',
+  'Bamna',
+  'Barguna-S',
+  'Betagi',
+  'Patharghata',
+  'Taltali',
+];
+
+List<String> barishalUpazilaList=[
+  'Agailjhara',
+  'Babuganj',
+  'Bakerganj',
+  'Banaripara',
+  'Barishal-S',
+  'Gouranadi',
+  'Hizla',
+  'Mehendiganj',
+  'Muladi',
+  'Uzirpur',
+];
+
+List<String> bholaUpazilaList=[
+  'Bhola-S',
+  'Borhanuddin',
+  'Charfassion',
+  'Daulatkhan',
+  'Lalmohan',
+  'Monpura',
+  'Tazumuddin',
+];
