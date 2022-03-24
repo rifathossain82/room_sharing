d912d79aabd76887d747b2ee1d72a226bea590d3
rifathossain82 <officialrifat82@gmail.com>
2022-03-24 09:02:35 +0600

    create upazila list

--- lib/constraints/strings.dart
+++ lib/constraints/strings.dart
@@ -141,3 +141,630 @@ List<String> bholaUpazilaList=[
   'Monpura',
   'Tazumuddin',
 ];
+
+List<String> BargunaUpazilaList=[
+'Amtali',
+'Bamna',
+'Barguna-S',
+'Betagi',
+'Patharghata',
+'Taltali',
+];
+List<String> BarisalUpazilaList=[
+'Agailjhara',
+'Babuganj',
+'Bakerganj',
+'Banaripara',
+'Barishal',
+'Gouranadi',
+'Hizla',
+'Mehendiganj',
+'Muladi',
+'Uzirpur',
+];
+List<String> BholaUpazilaList=[
+'Bhola- S',
+'Borhanuddin',
+'Charfassion',
+'Daulatkhan',
+'Lalmohan',
+'Monpura',
+'Tazumuddin',
+];
+List<String> JhalokathiUpazilaList=[
+'Jhalokathi-S',
+'Kathalia',
+'Nalchity',
+'Rajapur',
+];
+
+
+
+
+List<String> PatuakhaliUpazilaList=[
+'Bauphal',
+'Dashmina',
+'Dumki',
+'Galachipa',
+'Kalapara',
+'Mirjaganj',
+'Patuakhali-S',
+'Rangabali',
+];
+List<String> PirojpurUpazilaList=[
+'Bhandaria',
+'Kawkhali',
+'Mothbaria',
+'Nazirpur',
+'Nesarabad',
+'Pirojpur-S',
+'Zianagar',
+];
+List<String> B_bariaUpazilaList=[
+'Akhaura',
+'Ashuganj',
+'B_Baria-S',
+'Bancharampur',
+'Bijoynagar',
+'Kasba',
+'Nabinagar',
+'Nasirnagar',
+'Sarail',
+];
+List<String> BandarbanUpazilaList=[
+'Alikadam',
+'Bandarban-S',
+'Lama',
+'Naikhyongchari',
+'Rowangchari',
+'Ruma',
+'Thanchi',
+];
+List<String> ChandpurUpazilaList=[
+'Chandpur-S',
+'Faridganj',
+'Haimchar',
+'Haziganj',
+'Kachua',
+'Matlab(Dakshin)',
+'Matlab (Uttar)',
+'Shahrasti',
+];
+List<String> ChattogramUpazilaList=[
+'Anwara',
+'Banskhali',
+'Boalkhali',
+'Chandanish',
+'Fatikchari',
+'Hathazari',
+'Karnaphuli',
+'Lohagara',
+'Mirsharai',
+'Patiya',
+'Rangunia',
+'Raojan',
+'Sandwip',
+'Satkania',
+'Sitakunda',
+];
+List<String> Cox_bazarUpazilaList=[
+'Chakoria',
+'Cox\'S Bazar-S',
+'Kutubdia',
+'Moheskhali',
+'Pekua',
+'Ramu',
+'Teknaf',
+'Ukhiya',
+];
+
+
+List<String> CumillaUpazilaList=[
+'Barura',
+'Brahmanpara',
+'Burichong',
+'Chandina',
+'Chouddagram',
+'Cumilla-Sadar',
+'Cumilla-Sadar Daksin',
+'Daudkandi',
+'Debidwar',
+'Homna',
+'Laksham',
+'Lalmai',
+'Meghna',
+'Monohorganj',
+'Muradnagar',
+'Nangalkot',
+'Titas',
+];
+List<String> FeniUpazilaList=[
+'Chhagalniya',
+'Daganbhuiyan',
+'Feni-Sadar',
+'Fulgazi',
+'Porshuram',
+'Sonagazi',
+];
+List<String> KhagrachariUpazilaList=[
+'Dighinala',
+'Guimara',
+'Khagrachari-Sadar',
+'Laxmichari',
+'Mahalchari',
+'Manikchari',
+'Matiranga',
+'Panchari',
+'Ramgarh',
+];
+List<String> LaxmipurUpazilaList=[
+'Komol Nagar',
+'Laxmipur-Sadar',
+'Raipur',
+'Ramganj',
+'Ramgati',
+];
+List<String> NoakhaliUpazilaList=[
+'Begumganj',
+'Chatkhil',
+'Companiganj',
+'Hatiya',
+'Kabir Hat',
+'Noakhali-Sadar',
+'Senbag',
+'Sonaimuri',
+'Subarna Char',
+];
+List<String> RangamatiUpazilaList=[
+'Baghaichari',
+'Barkal',
+'Belaichari',
+'Juraichari',
+'Kaptai',
+'Kaukhali',
+'Langadu',
+'Nanniarchar',
+'Rajosthali',
+'Rangamati-Sadar',
+];
+List<String> DhakaUpazilaList=[
+'Dhamrai',
+'Dohar',
+'Keraniganj',
+'Nawabganj',
+'Savar',
+];
+List<String> FaridpurUpazilaList=[
+'Alfadanga',
+'Bhanga',
+'Boalmari',
+'Charbhadrasan',
+'Faridpur',
+'Madhukhali',
+'Nagarkanda',
+'Sadarpur',
+'Saltha',
+];
+List<String> GazipurUpazilaList=[
+'Gazipur-Sadar',
+'Kaliakoir',
+'Kaliganj',
+'Kapasia',
+'Sreepur',
+];
+List<String> GopalganjUpazilaList=[
+'Gopalganj-Sadar',
+'Kasiani',
+'Kotwalipara',
+'Muksudpur',
+'Tungipara',
+];
+List<String> KishoreganjUpazilaList=[
+'Austagram',
+'Bajitpur',
+'Bhairab',
+'Hossainpur',
+'Itna',
+'Karimganj',
+'Katiadi',
+'Kishoreganj- Sadar',
+'Kuliarchar',
+'Mithamoin',
+'Nikli',
+'Pakundia',
+'Tarail',
+];
+List<String> MadaripurUpazilaList=[
+'Kalkini',
+'Madaripur-Sadar',
+'Rajoir',
+'Shibchar',
+];
+List<String> ManikganjUpazilaList=[
+'Daulatpur',
+'Ghior',
+'Harirampur',
+'Manikganj',
+'Saturia',
+'Shivalaya',
+'Singai',
+];
+List<String> MunshiganjUpazilaList=[
+'Gazaria',
+'Lauhajong',
+'Munshiganj-Sadar',
+'Sirajdikhan',
+'Sreenagar',
+'Tongibari',
+];
+List<String> NarayanganjUpazilaList=[
+'Araihazar',
+'Bandar',
+'Narayanganj',
+'Rupganj',
+'Sonargaon',
+];
+List<String> NarshingdiUpazilaList=[
+'Belabo',
+'Monohardi',
+'Narshingdi-Sadar',
+'Palash',
+'Raipura',
+'Shibpur',
+];
+List<String> RajbariUpazilaList=[
+'Baliakandi',
+'Goalanda',
+'Kalukhali',
+'Pangsha',
+'Rajbari-Sadar',
+'Bhedarganj',
+];
+List<String> ShariatpurUpazilaList=[
+'Damuddya',
+'Goshairhat',
+'Janjira',
+'Naria',
+'Shariatpur-Sadar',
+];
+List<String> TangailUpazilaList=[
+'Basail',
+'Bhuapur',
+'Delduar',
+'Dhanbari',
+'Ghatail',
+'Gopalpur',
+'Kalihati',
+'Madhupur',
+'Mirzapur',
+'Nagarpur',
+'Shakhipur',
+'Tangail-Sadar',
+];
+List<String> BagerhatUpazilaList=[
+'Bagerhat-Sadar',
+'Chitalmari',
+'Fakirhat',
+'Kachua',
+'Mollahat',
+'Mongla',
+'Morrelganj',
+'Rampal',
+'Sharankhola',
+];
+List<String> ChuadangaUpazilaList=[
+'Sharankhola',
+'Alamdanga',
+'Chuadanga-Sadar',
+'Damurhuda',
+'Jibannagar',
+];
+List<String> JashoreUpazilaList=[
+'Abhoynagar',
+'Bagherpara',
+'Chowgacha',
+'Jashore-Sadar',
+'Jhikargacha',
+'Keshabpur',
+'Monirampur',
+'Sarsha',
+];
+List<String> JhenaidahUpazilaList=[
+'Harinakunda',
+'Jhenaidah-Sadar',
+'Kaliganj',
+'Kotchandpur',
+'Moheshpur',
+'Shailkupa',
+];
+List<String> KhulnaUpazilaList=[
+'Batiaghata',
+'Dacope',
+'Dighalia',
+'Dumuria',
+'Koira',
+'Paikgacha',
+'Phultala',
+'Rupsa',
+'Terokhada',
+];
+List<String> KushtiaUpazilaList=[
+'Bheramara',
+'Daulatpur',
+'Khoksha',
+'Kumarkhali',
+'Kushtia-Sadar',
+'Mirpur',
+];
+List<String> MaguraUpazilaList=[
+'Magura-Sadar',
+'Mohammadpur',
+'Salikha',
+'Sreepur',
+];
+List<String> MeherpurUpazilaList=[
+'Gangni',
+'Meherpur-Sadar',
+'Mujib Nagar',
+];
+List<String> NarailUpazilaList=[
+'Kalia',
+'Lohagara',
+'Narail-Sadar',
+];
+List<String> SatkhiraUpazilaList=[
+'Assasuni',
+'Debhata',
+'Kalaroa',
+'Kaliganj',
+'Satkhira-Sadar',
+'Shyamnagar',
+'Tala',
+];
+List<String> JamalpurUpazilaList=[
+'Bakshiganj',
+'Dewanganj',
+'Islampur',
+'Jamalpur-Sadar',
+'Madarganj',
+'Melendah',
+'Sarishabari',
+];
+List<String> MymensinghUpazilaList=[
+'Bhaluka',
+'Dhobaura',
+'Fulbaria',
+'Gaffargaon',
+'Gouripur',
+'Haluaghat',
+'Ishwarganj',
+'Muktagacha',
+'Mymensingh-Sadar',
+'Nandail',
+'Phulpur',
+'Tarakanda',
+'Trishal',
+];
+List<String> NetrokonaUpazilaList=[
+'Atpara',
+'Barhatta',
+'Durgapur',
+'Kalmakanda',
+'Khaliajuri',
+'Kendua',
+'Madan',
+'Mohanganj',
+'Netrakona-Sadar',
+'Purbadhala',
+];
+List<String> SherpurUpazilaList=[
+'Jhenaigati',
+'Nakla',
+'Nalitabari',
+'Sherpur-Sadar',
+'Sreebordi',
+];
+List<String> BoguraUpazilaList=[
+'Adamdighi',
+'Bogura-Sadar',
+'Dhunot',
+'Dhupchancia',
+'Gabtali',
+'Kahaloo',
+'Nandigram',
+'Sariakandi',
+'Shajahanpur',
+'Sherpur',
+'Shibganj',
+'Sonatala',
+];
+List<String> CapainawabganjUpazilaList=[
+'Bholahat',
+'Gomostapur',
+'Nachol',
+'Nawabganj-Sadar',
+'Shibganj',
+];
+List<String> JoypurhatUpazilaList=[
+'Akkelpur',
+'Joypurhat-Sadar',
+'Kalai',
+'Khetlal',
+'Panchbibi',
+];
+List<String> NaogaonUpazilaList=[
+'Atrai',
+'Badalgachi',
+'Dhamoirhat',
+'Manda',
+'Mohadevpur',
+'Naogaon-Sadar',
+'Niamatpur',
+'Patnitala',
+'Porsha',
+'Raninagar',
+'Shapahar',
+];
+List<String> NatoreUpazilaList=[
+'Bagatipara',
+'Baraigram',
+'Gurudaspur',
+'Lalpur',
+'Naldanga',
+'Natore-Sadar',
+'Singra',
+];
+List<String> PabnaUpazilaList=[
+'Atghoria',
+'Bera',
+'Bhangura',
+'Chatmohar',
+'Faridpur',
+'Ishwardi',
+'Pabna-Sadar',
+'Santhia',
+'Sujanagar',
+];
+List<String> RajshahiUpazilaList=[
+'Bagha',
+'Bagmara',
+'Charghat',
+'Durgapur',
+'Godagari',
+'Mohanpur',
+'Paba',
+'Puthia',
+'Tanore',
+];
+List<String> SirajganjUpazilaList=[
+'Belkuchi',
+'Chowhali',
+'Kamarkhand',
+'Kazipur',
+'Raiganj',
+'Shahzadpur',
+'Sirajganj-Sadar',
+'Tarash',
+'Ullapara',
+];
+List<String> DinajpurUpazilaList=[
+'Birampur',
+'Birganj',
+'Birol',
+'Bochaganj',
+'Chirirbandar',
+'Dinajpur-Sadar',
+'Fulbari',
+'Ghoraghat',
+'Hakimpur',
+'Kaharol',
+'Khanshama',
+'Nawabganj',
+'Parbatipur',
+];
+List<String> GaibandhaUpazilaList=[
+'Fulchari',
+'Gaibandha-Sadar',
+'Gobindaganj',
+'Palashbari',
+'Sadullapur',
+'Saghata',
+'Sundarganj',
+];
+List<String> KurigramUpazilaList=[
+'Bhurungamari',
+'Chilmari',
+'Fulbari',
+'Kurigram-Sadar',
+'Nageswari',
+'Rajarhat',
+'Rajibpur',
+'Rowmari',
+'Ulipur',
+];
+List<String> LalmonirhatUpazilaList=[
+'Aditmari',
+'Hatibandha',
+'Kaliganj',
+'Lalmonirhat-Sadar',
+'Patgram',
+];
+List<String> NilphamariUpazilaList=[
+'Dimla',
+'Domar',
+'Jaldhaka',
+'Kishoreganj',
+'Nilphamari-Sadar',
+'Sayedpur',
+];
+List<String> PanchagarhUpazilaList=[
+'Atwari',
+'Boda',
+'Debiganj',
+'Panchagarh-Sadar',
+'Tetulia',
+];
+List<String> RangpurUpazilaList=[
+'Badarganj',
+'Gangachara',
+'Kaunia',
+'Mithapukur',
+'Pirgacha',
+'Pirganj',
+'Rangpur-Sadar',
+'Taraganj',
+];
+List<String> ThakurgaonUpazilaList=[
+'Baliadangi',
+'Haripur',
+'Pirganj',
+'Ranisankail',
+'Thakurgaon-Sadar',
+];
+List<String> HabiganjUpazilaList=[
+'Azmiriganj',
+'Bahubal',
+'Baniachong',
+'Chunarughat',
+'Habiganj-Sadar',
+'Lakhai',
+'Madhabpur',
+'Nabiganj',
+'Sayestaganj',
+];
+List<String> MoulvibazarUpazilaList=[
+'Barlekha',
+'Juri',
+'Kamalganj',
+'Kulaura',
+'Moulvibazar-Sadar',
+'Rajnagar',
+'Sreemangal',
+];
+List<String> SunamganjUpazilaList=[
+'Biswamvarpur',
+'Chatak',
+'Dakhin Sunamganj',
+'Dharmapasha',
+'Doarabazar',
+'Jagannathpur',
+'Jamalganj',
+'Sulla',
+'Sunamganj-Sadar',
+'Tahirpur',
+];
+List<String> SylhetUpazilaList=[
+'Balaganj',
+'Beanibazar',
+'Biswanath',
+'Companiganj',
+'Dakshin Surma',
+'Fenchuganj',
+'Golapganj',
+'Gowainghat',
+'Jointiapur',
+'Kanaighat',
+'Osmaninagar',
+'Sylhet-Sadar',
+'Zakiganj',
+];
